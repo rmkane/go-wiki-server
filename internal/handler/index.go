57b9516c2850c1d000e578616c377e411931fce7
rmkane <rmkane@proton.me>
2025-03-07 12:42:39 -0500

    Fix the root handler

--- internal/handler/index.go
+++ internal/handler/index.go
@@ -1,32 +1,46 @@
 package handler
 
 import (
+	"log"
 	"net/http"
 	"path/filepath"
+	"strings"
 
 	"github.com/rmkane/go-wiki-server/internal/constants"
 	"github.com/rmkane/go-wiki-server/internal/render"
 )
 
+type Pages struct {
+	Pages []string
+}
+
 func RootHandler(w http.ResponseWriter, r *http.Request) {
-	files, err := filepath.Glob(filepath.Join(constants.DataDir, "*.txt"))
+	files, err := filepath.Glob(filepath.Join(constants.DataDir, "*.md"))
 	if err != nil {
 		http.Error(w, "Unable to load pages", http.StatusInternalServerError)
 		return
 	}
 
-	var pages []string
-	for _, file := range files {
-		title := filepath.Base(file) // Get filename
-		title = title[:len(title)-4] // Remove ".txt" extension
-		pages = append(pages, title)
-	}
+	pages := getPageTitles(files)
 
-	data := struct {
-		Pages []string
-	}{
+	log.Println("Pages:", pages)
+
+	data := Pages{
 		Pages: pages,
 	}
 
 	render.RenderTemplate(w, "index", data)
+}
+
+func getPageTitles(files []string) []string {
+	var titles = make([]string, len(files))
+	for i, file := range files {
+		titles[i] = getPageTitle(file)
+	}
+	return titles
+}
+
+func getPageTitle(file string) string {
+	basename := filepath.Base(file)
+	return strings.TrimSuffix(basename, filepath.Ext(basename))
 }
